c11f291531853586bbf73df6318bead045608f77
SAI SNEHA S V <46928434+saisnehasv@users.noreply.github.com>
2021-04-26 08:42:46 +0530

    Week 1 update

--- src/process.py
+++ src/process.py
@@ -1,9 +1,3 @@
-from lipnet.lipreading.videos import Video
-from lipnet.lipreading.visualization import show_video_subtitle
-from lipnet.core.decoders import Decoder
-from lipnet.lipreading.helpers import labels_to_text
-from lipnet.utils.spell import Spell
-from lipnet.model2 import LipNet
 from keras.optimizers import Adam
 import ffmpeg
 from keras import backend as K
@@ -15,15 +9,11 @@ np.random.seed(55)
 
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 
-FACE_PREDICTOR_PATH = os.path.join(CURRENT_PATH,'..','common','predictors','shape_predictor_68_face_landmarks.dat')
-
 PREDICT_GREEDY      = False
 PREDICT_BEAM_WIDTH  = 200
-PREDICT_DICTIONARY  = os.path.join(CURRENT_PATH,'..','common','dictionaries','grid.txt')
 
 def predict(weight_path, video_path, absolute_max_string_len=32, output_size=28):
     print ("\nLoading data from disk...")
-    video = Video(vtype='face', face_predictor_path=FACE_PREDICTOR_PATH)
     if os.path.isfile(video_path):
         video.from_video(video_path)
     else:
@@ -35,26 +25,11 @@ def predict(weight_path, video_path, absolute_max_string_len=32, output_size=28)
     else:
         frames_n, img_w, img_h, img_c = video.data.shape
 
-
-    lipnet = LipNet(img_c=img_c, img_w=img_w, img_h=img_h, frames_n=frames_n,
-                    absolute_max_string_len=absolute_max_string_len, output_size=output_size)
-
     adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-
-    lipnet.model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=adam)
-    lipnet.model.load_weights(weight_path)
-
-    spell = Spell(path=PREDICT_DICTIONARY)
-    decoder = Decoder(greedy=PREDICT_GREEDY, beam_width=PREDICT_BEAM_WIDTH,
-                      postprocessors=[labels_to_text, spell.sentence])
 
     X_data       = np.array([video.data]).astype(np.float32) / 255
     input_length = np.array([len(video.data)])
 
-    y_pred         = lipnet.predict(X_data)
-    result         = decoder.decode(y_pred, input_length)[0]
-
-    return (video, result)
 
 if __name__ == '__main__':
     if len(sys.argv) == 3:
